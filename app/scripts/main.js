56faab095ae30a4fb6aed81ae80e0968e18ee88a
handres <maesegorak@gmail.com>
2015-06-03 16:22:26 +0200

    email comprobado con base de datos

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -143,30 +143,7 @@ $.validator.addMethod("iban", function(value, element) {
 
     // calculate the result of: ibancheckdigits % 97
     for (p = 0; p < ibancheckdigits.length; p++) {
-        cChar = ibancheckdigits.charAt(p);
-        cOperator = "" + cRest + "" + cChar;
-        cRest = cOperator % 97;
-    }
-    return cRest === 1;
-}, "introduce un  IBAN correcto");
-
-
-});
-$.extend($.validator.messages, {
-    required: "Este campo es obligatorio.",
-    remote: "Este usuario esta ocupado elija otro.",
-    email: "Por favor, escribe una dirección de correo válida.",
-    url: "Por favor, escribe una URL válida.",
-    date: "Por favor, escribe una fecha válida.",
-    dateISO: "Por favor, escribe una fecha (ISO) válida.",
-    number: "Por favor, escribe un número válido.",
-    digits: "Por favor, escribe sólo dígitos.",
-    creditcard: "Por favor, escribe un número de tarjeta válido.",
-    equalTo: "Por favor, escribe el mismo valor de nuevo.",
-    extension: "Por favor, escribe un valor con una extensión aceptada.",
-    maxlength: $.validator.format("Por favor, no escribas más de {0} caracteres."),
-    minlength: $.validator.format("Por favor, no escribas menos de {0} caracteres."),
-    rangelength: $.validator.format("Por favor, escribe un valor entre {0} y {1} caracteres."),
+        cChar = ibancheckdigits.charAt(p);cds."),
     range: $.validator.format("Por favor, escribe un valor entre {0} y {1}."),
     max: $.validator.format("Por favor, escribe un valor menor o igual a {0}."),
     min: $.validator.format("Por favor, escribe un valor mayor o igual a {0}."),
