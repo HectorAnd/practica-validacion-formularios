145c5ee0c12c49b69e42c1a12fdf3bf6713e6f5d
handres <maesegorak@gmail.com>
2015-06-25 17:03:00 +0200

    todo corregido

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -242,7 +242,8 @@ $.extend($.validator.messages, {
   min: $.validator.format("Por favor, escribe un valor mayor o igual a {0}."),
   nifES: "Por favor, escribe un NIF válido.",
   nieES: "Por favor, escribe un NIE válido.",
-  cifES: "Por favor, escribe un CIF válido."
+  cifES: "Por favor, escribe un CIF válido.",
+  iban:"Por favor introduce un iban valido."
 });
 
 
